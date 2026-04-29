8e2c9fa50b8092c338030f89fcaefc3cc0e0de4d
Juice Boy <justinbell0114@gmail.com>
2026-04-28 23:39:39 -0400

    broken rtl code but interesting ideas

--- warp_scheduler.sv
+++ warp_scheduler.sv
@@ -2,9 +2,16 @@
 module warp_scheduler (
 input logic clk,
 input logic reset_n,
+input logic [11:0] pc,
 output logic state);
 
+enum logic [8:0] IDLE, SELECT, conflict, FETCH, DECODE, EXECUTE, ISSUE, WAIT_EU, WRITEBACK, ADVANCE;
 
+always_ff @(posedge clk, negedge reset_n) begin
+    if(!reset_n) begin
+        
+    end
+end
 
 
 
